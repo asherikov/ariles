6ef14708a306bd3c07ecc735b6eed5405ca4d130
asherikov <asherikov@yandex.com>
2018-06-06 22:29:30 +0300

    Refactoring of ariles_add_external_git_project.cmake

--- cmake/ariles_add_external_git_project.cmake
+++ cmake/ariles_add_external_git_project.cmake
@@ -5,55 +5,39 @@ function(ariles_add_external_git_project)
     cmake_parse_arguments("ARILES" "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
 
 
+    set (EXTERNAL_PROJECT_PARAMETERS
+        ${ARILES_TARGET_NAME}
+        DOWNLOAD_DIR    "${ARILES_PROJECT_DIR}"
+        SOURCE_DIR      "${ARILES_PROJECT_DIR}"
+        STEP_TARGETS    download)
+
+
+    if (ARILES_NOBUILD)
+        list(APPEND EXTERNAL_PROJECT_PARAMETERS
+            BUILD_COMMAND       ""
+            INSTALL_COMMAND     ""
+            CONFIGURE_COMMAND   ""
+        )
+    else (ARILES_NOBUILD)
+        list(APPEND EXTERNAL_PROJECT_PARAMETERS
+            CMAKE_ARGS      ${ARILES_CMAKE_ARGS}
+        )
+    endif (ARILES_NOBUILD)
+
+
     if (EXISTS "${ARILES_PROJECT_DIR}/.git")
         message("Found '.git' subdirectory in project '${ARILES_PROJECT_DIR}': skipping download step.")
 
-        if (ARILES_NOBUILD)
-            ExternalProject_Add(
-                ${ARILES_TARGET_NAME}
-                DOWNLOAD_DIR    "${ARILES_PROJECT_DIR}"
-                SOURCE_DIR      "${ARILES_PROJECT_DIR}"
-                DOWNLOAD_COMMAND    ""
-                BUILD_COMMAND       ""
-                INSTALL_COMMAND     ""
-                CONFIGURE_COMMAND   ""
-                STEP_TARGETS    download
-            )
-        else (ARILES_NOBUILD)
-            ExternalProject_Add(
-                ${ARILES_TARGET_NAME}
-                DOWNLOAD_DIR    "${ARILES_PROJECT_DIR}"
-                SOURCE_DIR      "${ARILES_PROJECT_DIR}"
-                CMAKE_ARGS      ${ARILES_CMAKE_ARGS}
-                DOWNLOAD_COMMAND    ""
-                STEP_TARGETS    download
-            )
-        endif (ARILES_NOBUILD)
+        list(APPEND EXTERNAL_PROJECT_PARAMETERS
+            DOWNLOAD_COMMAND    ""
+        )
     else()
         find_package(Git)
 
         if (GIT_FOUND)
-            if (ARILES_NOBUILD)
-                ExternalProject_Add(
-                    ${ARILES_TARGET_NAME}
-                    DOWNLOAD_DIR    "${ARILES_PROJECT_DIR}"
-                    SOURCE_DIR      "${ARILES_PROJECT_DIR}"
-                    DOWNLOAD_COMMAND    ${GIT_EXECUTABLE} submodule update --init --no-fetch ./
-                    BUILD_COMMAND       ""
-                    INSTALL_COMMAND     ""
-                    CONFIGURE_COMMAND   ""
-                    STEP_TARGETS    download
-                )
-            else (ARILES_NOBUILD)
-                ExternalProject_Add(
-                    ${ARILES_TARGET_NAME}
-                    DOWNLOAD_DIR    "${ARILES_PROJECT_DIR}"
-                    SOURCE_DIR      "${ARILES_PROJECT_DIR}"
-                    CMAKE_ARGS      ${ARILES_CMAKE_ARGS}
-                    DOWNLOAD_COMMAND    ${GIT_EXECUTABLE} submodule update --init --no-fetch ./
-                    STEP_TARGETS    download
-                )
-            endif (ARILES_NOBUILD)
+            list(APPEND EXTERNAL_PROJECT_PARAMETERS
+                DOWNLOAD_COMMAND    ${GIT_EXECUTABLE} submodule update --init --no-fetch ./
+            )
         else(GIT_FOUND)
             message( SEND_ERROR "Cannot download project '${ARILES_PROJECT_DIR}' due to missing git. Checking if the files are alredy there..." )
             file(GLOB RESULT ${ARILES_PROJECT_DIR})
@@ -63,6 +47,10 @@ function(ariles_add_external_git_project)
             endif()
         endif(GIT_FOUND)
     endif()
+
+    ExternalProject_Add(
+        ${EXTERNAL_PROJECT_PARAMETERS}
+    )
 
 
     add_dependencies(${ARILES_TARGET_NAME}    TGT_fetch_bridges)
