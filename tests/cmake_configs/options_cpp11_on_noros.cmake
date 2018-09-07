3fb7afc0b1fa6f3ec93df32f0121c45dbfa89fec
asherikov <asherikov@yandex.com>
2018-09-07 23:15:41 +0300

    Travis cfg update.

--- tests/cmake_configs/options_cpp11_on_noros.cmake
+++ tests/cmake_configs/options_cpp11_on_noros.cmake
@@ -1,8 +1,8 @@
 set(ARILES_CPP_STANDARD         "c++11" CACHE STRING "")
 
 set(ARILES_BRIDGE_pugixml       "ON"    CACHE STRING "")
-set(ARILES_BRIDGE_rapidjson     "ON"    CACHE STRING "")
-set(ARILES_BRIDGE_jsonnet       "ON"    CACHE STRING "")
+set(ARILES_BRIDGE_rapidjson     "OFF"    CACHE STRING "")
+set(ARILES_BRIDGE_jsonnet       "OFF"   CACHE STRING "")
 set(ARILES_BRIDGE_msgpack       "ON"    CACHE STRING "")
 set(ARILES_BRIDGE_yaml_cpp03    "OFF"   CACHE STRING "")
 set(ARILES_BRIDGE_yaml_cpp      "ON"    CACHE STRING "")
