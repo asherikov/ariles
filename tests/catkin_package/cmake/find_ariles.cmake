7055af6b9b4fdc118704a7863320b33e1fc4feb6
asherikov <asherikov@yandex.com>
2019-04-19 16:55:20 +0200

    Minor fix in catkin_package/cmake/find_ariles.cmake

--- tests/catkin_package/cmake/find_ariles.cmake
+++ tests/catkin_package/cmake/find_ariles.cmake
@@ -1,6 +1,6 @@
 foreach(ARILES_COMPONENT  core yaml-cpp octave ros)
     find_package(ariles-${ARILES_COMPONENT} REQUIRED)
 
-    list(APPEND pal_ariles_LIBRARIES     "${ariles-${ARILES_COMPONENT}_LIBRARIES}")
-    list(APPEND pal_ariles_INCLUDE_DIRS  "${ariles-${ARILES_COMPONENT}_INCLUDE_DIRS}")
+    list(APPEND catkin_ariles_LIBRARIES     "${ariles-${ARILES_COMPONENT}_LIBRARIES}")
+    list(APPEND catkin_ariles_INCLUDE_DIRS  "${ariles-${ARILES_COMPONENT}_INCLUDE_DIRS}")
 endforeach()
