5bac1be40802a7f62a846719dcfc342e7dc782a6
asherikov <asherikov@yandex.com>
2020-09-23 21:36:06 +0200

    2: migration from 1.X to 2.X

--- 2/search/files_b.js
+++ 2/search/files_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['methods_2eh',['methods.h',['../methods_8h.html',1,'']]],
+  ['migration_5f1to2_2emd',['migration_1to2.md',['../migration__1to2_8md.html',1,'']]],
   ['msgpack_2eh',['msgpack.h',['../msgpack_8h.html',1,'']]]
 ];
--- 2/search/searchdata.js
+++ 2/search/searchdata.js
@@ -11,7 +11,7 @@ var indexSectionsWithContent =
   8: "agimsuv",
   9: "ar",
   10: "cdfgjmnoprswy",
-  11: "al"
+  11: "alm"
 };
 
 var indexSectionNames =
