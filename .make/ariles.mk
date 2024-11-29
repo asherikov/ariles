c43b0de7546d4a4fd114db478cc75d2469e6e4fd
Alexander Sherikov <alexander@sherikov.net>
2024-11-29 10:19:00 +0400

    Hide ariles source directory with CATKIN_IGNORE

--- .make/ariles.mk
+++ .make/ariles.mk
@@ -13,6 +13,7 @@ update:
 	git rm --ignore-unmatch -rf ariles
 	git read-tree --prefix=ariles -u ${VERSION}
 	${MAKE} cleanup
+	touch ariles/CATKIN_IGNORE
 
 update_head:
 	${MAKE} update VERSION="origin/head_2"
