b03ae70cc36c4a19931cbfe59ab4d8eacbb3aa81
Alexander Sherikov <alexander@sherikov.net>
2024-12-23 22:16:36 +0400

    Prepare 2.3.3

---
+++ cmake/options_deb_packages_noble.cmake
@@ -0,0 +1,8 @@
+set(ARILES_DEB_TARGETS              "noble"             CACHE STRING "")
+
+set(ARILES_BUILD_REGRESSION_TESTS   "OFF"               CACHE STRING "")
+
+
+set(ARILES_VISITORS_DEFAULT_MODE    "ON"                CACHE STRING "")
+set(ARILES_VISITOR_rosparam         "OFF"               CACHE STRING "")
+set(ARILES_VISITOR_ros2param        "$ENV{ROS_DISTRO}"  CACHE STRING "")
